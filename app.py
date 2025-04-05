e2e333530c0e139f3b351f2e63825844e91fbf1c
mA1243 <ajmary2005@gmail.com>
2025-04-06 00:24:03 +0100

    Add temp

--- app.py
+++ app.py
@@ -1,7 +1,50 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
+import requests
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+def get_location(city):
+    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}")
+    response.raise_for_status()
+    data = response.json()
+    location = {
+        "lat": data[0]["lat"],
+        "lon" : data[0]["lon"]
+    }
+    return location
+
+def get_weather_data(lat, lon):
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric")
+    response.raise_for_status()
+    data = response.json()
+    return data
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+@app.route("/getWeather")
+def get_weather():
+    try:
+        city = request.args.get("city")
+        location = get_location(city)
+        lat = location["lat"]
+        lon = location["lon"]
+        weather_data = get_weather_data(lat, lon)
+        temp = weather_data["main"]["temp"]
+        unit = "celsius"
+        current_weather = {
+            "temperature": {
+                "value": temp,
+                "unit": unit
+            }
+        }
+        return jsonify(current_weather)
+    except:
+        return "City Not Found"
